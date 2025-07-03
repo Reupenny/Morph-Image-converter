c626f64cee1b90764acb32d7677e51e96c3f7d8a
Rdavern <me@reubendavern.com>
2025-07-03 23:47:06 +1200

    electron app setup

---
+++ App/electron/main.js
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        minWidth: 500,
+        minHeight: 700,
+        width: 500,
+        height: 700,
+        webPreferences: {
+            preload: path.join(__dirname, '../electron/preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+    // mainWindow.webContents.openDevTools(); // this is optional thing, use it if you see a devTool window opened
+}
+
+app.whenReady().then(() => {
+    //additional logic here
+}).then(createWindow)
+
+app.on('window-all-closed', () => {
+    // eslint-disable-next-line no-undef
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
